c494c076c74be1479bce33391711b9217fd8feae
MpaK <mrak69@gmail.com>
2013-12-18 20:05:42 +0700

    Domain name changed

--- javascripts/like.js
+++ javascripts/like.js
@@ -8,8 +8,8 @@
 
     Like.prototype.show = function() {
       var domain, title;
-      title = 'Middleman';
-      domain = 'http://mydomain.com';
+      title = 'aomega.ru';
+      domain = 'http://aomegaru.github.io/';
       return this.find('msg').innerHTML = "    	<a target=\"_blank\" href=\"https://twitter.com/intent/tweet?text=" + title + "&url=" + domain + "&via=TWITTER-HANDLE\" class=\"btn-twitter\">Twitter</a>\n    	<a target=\"_blank\" href=\"http://www.facebook.com/sharer/sharer.php?u=" + domain + "\" class=\"btn-facebook\">Facebook</a>\n<a target=\"_blank\" href=\"https://vk.com/share.php?url=" + domain + "&title=" + title + "&noparse=true\" class=\"btn-vk\">VK</a>";
     };
 
